b50ed11564461dcf332a0c85553c1fc22f94cc15
Andre Hartmann <andre.hartmann@zalando.de>
2016-08-09 18:49:32 +0200

    #5 upgrade versions

--- src/test/java/de/zalando/spring/cloud/config/aws/kms/DisabledKmsEncryptionIntegrationConfigurationTest.java
+++ src/test/java/de/zalando/spring/cloud/config/aws/kms/DisabledKmsEncryptionIntegrationConfigurationTest.java
@@ -15,41 +15,39 @@
  */
 package de.zalando.spring.cloud.config.aws.kms;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
-
-import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This integration test shows the usage of "aws.kms.enabled=false" to completely disable KMS, although there are
- * encrypted properties. This may be useful for local development.
+ * encrypted properties. This may be useful for local development. Please note the "encrypt.failOnError=false" property.
+ * If absent the application startup would fail fast, if any cipher-property cannot be decrypted.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration
-@IntegrationTest("aws.kms.enabled=false")
+@SpringBootTest({"aws.kms.enabled=false", "encrypt.failOnError=false"})
 @ActiveProfiles("encryption")
 public class DisabledKmsEncryptionIntegrationConfigurationTest {
 
-    static final String ENCRYPTED = "{cipher}c2VjcmV0";
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
     @Value("${secret}")
     private String decryptedSecret;
 
     @Test
     public void testPropertyHasNotBeenDecrypted() throws Exception {
-        assertThat(decryptedSecret).isEqualTo(ENCRYPTED);
+        assertThat(decryptedSecret).isEmpty();
     }
 
     @Configuration
--- src/test/java/de/zalando/spring/cloud/config/aws/kms/KmsEncryptionIntegrationConfigurationTest.java
+++ src/test/java/de/zalando/spring/cloud/config/aws/kms/KmsEncryptionIntegrationConfigurationTest.java
@@ -15,45 +15,42 @@
  */
 package de.zalando.spring.cloud.config.aws.kms;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.verify;
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.model.DecryptRequest;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import java.nio.ByteBuffer;
 
-import org.junit.Test;
-
-import org.junit.runner.RunWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.amazonaws.services.kms.AWSKMS;
-import com.amazonaws.services.kms.model.DecryptRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 /**
  * This integration test shows the usage of spring-cloud-config-aws-kms. You will find an encrypted property within
  * src/test/resources/ that will be decrypted during the bootstrap phase. In order to make this test runnable on every
  * machine, a mock is used instead of a real AWSKMSClient.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration
-@IntegrationTest
+@SpringBootTest
 @ActiveProfiles("encryption")
 public class KmsEncryptionIntegrationConfigurationTest {
 
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
     static final String PLAINTEXT = "Hello World";
-    static final ByteBuffer CIPHER_TEXT_BLOB = ByteBuffer.wrap("secret".getBytes());
+    private static final ByteBuffer CIPHER_TEXT_BLOB = ByteBuffer.wrap("secret".getBytes());
 
     @Autowired
     private AWSKMS mockKms;
