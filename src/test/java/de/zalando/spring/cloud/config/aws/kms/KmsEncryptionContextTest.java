4087f84aaea37da0dd4742845851fc509f5816f4
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-02-18 22:26:38 +1300

    Simplify test code with AssertJ

--- src/test/java/de/zalando/spring/cloud/config/aws/kms/KmsEncryptionContextTest.java
+++ src/test/java/de/zalando/spring/cloud/config/aws/kms/KmsEncryptionContextTest.java
@@ -15,10 +15,9 @@
  */
 package de.zalando.spring.cloud.config.aws.kms;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 import static org.junit.Assert.assertEquals;
-
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.junit.Test;
 
@@ -29,26 +28,14 @@ public class KmsEncryptionContextTest {
 
     private static final String ENCRYPTION_TEXT = "(param=TOKAmWhvbW1lIGPigJllc3Qgcmllbg==,"
             + "test=bOKAmW9ldXZyZSBj4oCZZXN0IHRvdXQ= ,valueless)remaining";
-    private static final int NUMBER_OF_ENTRIES = 3;
 
     @Test
     public void testExtractEncryptionContext() {
-        Map<String, String> encryptionContext = KmsTextEncryptor.extractEncryptionContext(ENCRYPTION_TEXT);
-        assertEquals(NUMBER_OF_ENTRIES, encryptionContext.size());
-        int found = 0;
-        for (Entry<String, String> entry : encryptionContext.entrySet()) {
-            if ("param".equals(entry.getKey())) {
-                assertEquals("L’homme c’est rien", entry.getValue());
-                found++;
-            } else if ("test".equals(entry.getKey())) {
-                assertEquals("l’oeuvre c’est tout", entry.getValue());
-                found++;
-            } else if ("valueless".equals(entry.getKey())) {
-                assertEquals("An empty value must still be added!", "", entry.getValue());
-                found++;
-            }
-        }
-        assertEquals("Test did not find right number of key and value pairs", NUMBER_OF_ENTRIES, found);
+        assertThat(KmsTextEncryptor.extractEncryptionContext(ENCRYPTION_TEXT))
+                .containsOnly(
+                    entry("param", "L’homme c’est rien"),
+                    entry("test", "l’oeuvre c’est tout"),
+                    entry("valueless", ""));
     }
 
     @Test
