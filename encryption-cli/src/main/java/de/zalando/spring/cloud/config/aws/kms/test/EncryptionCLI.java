c925646246be694b26a25723f42da053312f01ae
Andre Hartmann <andre.hartmann@zalando.de>
2017-11-14 15:35:26 +0100

    update encryption cli

--- encryption-cli/src/main/java/de/zalando/spring/cloud/config/aws/kms/test/EncryptionCLI.java
+++ encryption-cli/src/main/java/de/zalando/spring/cloud/config/aws/kms/test/EncryptionCLI.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2015 Zalando SE (https://tech.zalando.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package de.zalando.spring.cloud.config.aws.kms.test;
 
 import com.amazonaws.services.kms.AWSKMS;
@@ -29,16 +14,16 @@ public class EncryptionCLI implements CommandLineRunner {
     @Override
     public void run(final String... args) {
         try {
-            checkArgument(args.length >= 3, "Too few arguments.");
+            checkArgument(args.length >= 2, "Too few arguments.");
 
             final String text = args[1];
-            final AWSKMS kms = AWSKMSClientBuilder.standard().withRegion(args[2]).build();
+            final AWSKMS kms = AWSKMSClientBuilder.defaultClient();
 
             switch (args[0]) {
 
                 case "encrypt" :
-                    checkArgument(args.length == 4, "Too few arguments.");
-                    System.out.println(new KmsTextEncryptor(kms, args[3]).encrypt(text));
+                    checkArgument(args.length == 3, "Too few arguments.");
+                    System.out.println(new KmsTextEncryptor(kms, args[2]).encrypt(text));
                     break;
 
                 case "decrypt" :
@@ -50,9 +35,13 @@ public class EncryptionCLI implements CommandLineRunner {
                     break;
             }
         } catch (final IllegalArgumentException e) {
-            System.out.println(e.getMessage() + " Usage:\n"                  //
-                    + "./run.sh encrypt 'plaintext' eu-west-1 ${kmsKeyId}\n" //
-                    + "./run.sh decrypt 'base64cipherText' eu-west-1");
+            System.out.println(e.getMessage() + " Usage:\n"        //
+                    + "Make sure a region is set, either in ~/.aws/config " //
+                    + "or with `export AWS_REGION=eu-central-1`\n" //
+                    + "\n" //
+                    + "then do\n" //
+                    + "./run.sh encrypt 'plaintext' ${kmsKeyId}\n" //
+                    + "./run.sh decrypt 'base64cipherText'");
         }
     }
 }
