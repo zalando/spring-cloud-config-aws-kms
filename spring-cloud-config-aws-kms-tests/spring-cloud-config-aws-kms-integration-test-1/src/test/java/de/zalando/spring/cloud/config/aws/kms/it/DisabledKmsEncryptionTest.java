b313790f2d34e1805dbe3c34a2e3319f4d00ac07
André Menneken <andre@zalando.de>
2020-04-28 16:02:53 +0200

    re-arranged tests
    Signed-off-by: André Menneken <andre@zalando.de>

--- spring-cloud-config-aws-kms-tests/spring-cloud-config-aws-kms-integration-test-1/src/test/java/de/zalando/spring/cloud/config/aws/kms/it/DisabledKmsEncryptionTest.java
+++ spring-cloud-config-aws-kms-tests/spring-cloud-config-aws-kms-integration-test-1/src/test/java/de/zalando/spring/cloud/config/aws/kms/it/DisabledKmsEncryptionTest.java
@@ -1,15 +1,9 @@
 package de.zalando.spring.cloud.config.aws.kms.it;
 
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,16 +16,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("encryption")
 public class DisabledKmsEncryptionTest {
 
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
     @Value("${secret}")
     private String decryptedSecret;
 
     @Test
-    public void testPropertyHasNotBeenDecrypted() throws Exception {
+    public void testPropertyHasNotBeenDecrypted() {
         assertThat(decryptedSecret).isEmpty();
     }
 }
--- spring-cloud-config-aws-kms-tests/spring-cloud-config-aws-kms-integration-test-1/src/test/java/de/zalando/spring/cloud/config/aws/kms/it/KmsEncryptionTest.java
+++ spring-cloud-config-aws-kms-tests/spring-cloud-config-aws-kms-integration-test-1/src/test/java/de/zalando/spring/cloud/config/aws/kms/it/KmsEncryptionTest.java
@@ -3,15 +3,11 @@ package de.zalando.spring.cloud.config.aws.kms.it;
 import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.model.DecryptRequest;
 import de.zalando.spring.cloud.config.aws.kms.MockAwsKmsConfig;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import java.nio.ByteBuffer;
 
@@ -27,11 +23,6 @@ import static org.mockito.Mockito.verify;
 @SpringBootTest
 @ActiveProfiles("encryption")
 public class KmsEncryptionTest {
-
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
     private static final ByteBuffer CIPHER_TEXT_BLOB = ByteBuffer.wrap("secret".getBytes());
 
