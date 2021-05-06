1fee15ca0d69c3f437d335d282c3f2721721b611
Roger Viñas <roger.vinas@adevinta.com>
2021-05-06 19:39:18 +0200

    Upgrade to JUnit5
    Signed-off-by: Roger Viñas <roger.vinas@adevinta.com>

--- library/src/test/java/de/zalando/spring/cloud/config/aws/kms/EncryptedTokenTest.java
+++ library/src/test/java/de/zalando/spring/cloud/config/aws/kms/EncryptedTokenTest.java
@@ -1,20 +1,19 @@
 package de.zalando.spring.cloud.config.aws.kms;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static de.zalando.spring.cloud.config.aws.kms.OutputMode.BASE64;
 import static de.zalando.spring.cloud.config.aws.kms.OutputMode.PLAIN;
-import static java.util.Arrays.asList;
 import static java.util.Collections.emptyMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(Parameterized.class)
 public class EncryptedTokenTest {
 
     private static final KmsTextEncryptorOptions BASE64_OPTIONS = KmsTextEncryptorOptions.builder().withOutputMode(BASE64).build();
@@ -29,34 +28,24 @@ public class EncryptedTokenTest {
         CONTEXT_MAP.put("valueless", "");
     }
 
-    private final String testString;
-    private final KmsTextEncryptorOptions expectedOptions;
-    private final Map<String,String> expectedContext;
-
-    public EncryptedTokenTest(String testString, KmsTextEncryptorOptions expectedOptions, Map<String, String> expectedContext) {
-        this.testString = testString;
-        this.expectedOptions = expectedOptions;
-        this.expectedContext = expectedContext;
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of("(param=TOKAmWhvbW1lIGPigJllc3Qgcmllbg==,test=bOKAmW9ldXZyZSBj4oCZZXN0IHRvdXQ= ,valueless)[foo=bar,output=base64]SGVsbG8gV29ybGQ=", BASE64_OPTIONS, CONTEXT_MAP),
+                Arguments.of("[foo=bar,output=base64](param=TOKAmWhvbW1lIGPigJllc3Qgcmllbg==,test=bOKAmW9ldXZyZSBj4oCZZXN0IHRvdXQ= ,valueless)SGVsbG8gV29ybGQ=", BASE64_OPTIONS, CONTEXT_MAP),
+                Arguments.of("(param=TOKAmWhvbW1lIGPigJllc3Qgcmllbg==,test=bOKAmW9ldXZyZSBj4oCZZXN0IHRvdXQ= ,valueless)SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, CONTEXT_MAP),
+                Arguments.of("[foo=bar,output=base64]SGVsbG8gV29ybGQ=", BASE64_OPTIONS, EMPTY_MAP),
+                Arguments.of("SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP),
+                Arguments.of("()[]SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP),
+                Arguments.of("[]()SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP),
+                Arguments.of("()SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP),
+                Arguments.of("[]SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP),
+                Arguments.of("SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP)
+        );
     }
 
-    @Parameterized.Parameters(name = "{index}: fib[{0}]={1}")
-    public static Iterable<Object[]> data() {
-        return asList(new Object[][]{
-                {"(param=TOKAmWhvbW1lIGPigJllc3Qgcmllbg==,test=bOKAmW9ldXZyZSBj4oCZZXN0IHRvdXQ= ,valueless)[foo=bar,output=base64]SGVsbG8gV29ybGQ=", BASE64_OPTIONS, CONTEXT_MAP},
-                {"[foo=bar,output=base64](param=TOKAmWhvbW1lIGPigJllc3Qgcmllbg==,test=bOKAmW9ldXZyZSBj4oCZZXN0IHRvdXQ= ,valueless)SGVsbG8gV29ybGQ=", BASE64_OPTIONS, CONTEXT_MAP},
-                {"(param=TOKAmWhvbW1lIGPigJllc3Qgcmllbg==,test=bOKAmW9ldXZyZSBj4oCZZXN0IHRvdXQ= ,valueless)SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, CONTEXT_MAP},
-                {"[foo=bar,output=base64]SGVsbG8gV29ybGQ=", BASE64_OPTIONS, EMPTY_MAP},
-                {"SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP},
-                {"()[]SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP},
-                {"[]()SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP},
-                {"()SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP},
-                {"[]SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP},
-                {"SGVsbG8gV29ybGQ=", PLAIN_OPTIONS, EMPTY_MAP}
-        });
-    }
-
-    @Test
-    public void testParseToken() {
+    @ParameterizedTest(name = "{index}: fib[{0}]={1}")
+    @MethodSource("data")
+    public void testParseToken(String testString, KmsTextEncryptorOptions expectedOptions, Map<String, String> expectedContext) {
         final EncryptedToken token = EncryptedToken.parse(testString);
 
         assertThat(token.getCipherBytes()).isEqualTo(ByteBuffer.wrap("Hello World".getBytes()));
