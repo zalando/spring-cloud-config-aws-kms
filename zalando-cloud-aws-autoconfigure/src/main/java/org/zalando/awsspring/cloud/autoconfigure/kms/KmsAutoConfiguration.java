9ed0d5b60989683c37b5158474d208f126a0961c
drohe <daniel.rohe@zalando.de>
2025-03-14 08:58:47 +0100

    added service-connection capability

--- zalando-cloud-aws-autoconfigure/src/main/java/org/zalando/awsspring/cloud/autoconfigure/kms/KmsAutoConfiguration.java
+++ zalando-cloud-aws-autoconfigure/src/main/java/org/zalando/awsspring/cloud/autoconfigure/kms/KmsAutoConfiguration.java
@@ -1,40 +1,65 @@
 package org.zalando.awsspring.cloud.autoconfigure.kms;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
+import io.awspring.cloud.autoconfigure.AwsAsyncClientCustomizer;
+import io.awspring.cloud.autoconfigure.AwsSyncClientCustomizer;
 import io.awspring.cloud.autoconfigure.core.AwsClientBuilderConfigurer;
 import io.awspring.cloud.autoconfigure.core.AwsClientCustomizer;
+import io.awspring.cloud.autoconfigure.core.AwsConnectionDetails;
+import io.awspring.cloud.autoconfigure.core.CredentialsProviderAutoConfiguration;
+import io.awspring.cloud.autoconfigure.core.RegionProviderAutoConfiguration;
 import software.amazon.awssdk.services.kms.KmsAsyncClient;
 import software.amazon.awssdk.services.kms.KmsAsyncClientBuilder;
 import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.kms.KmsClientBuilder;
 
-@Configuration
-@ConditionalOnClass({ KmsClient.class, KmsAsyncClient.class })
+@AutoConfiguration
 @EnableConfigurationProperties({ KmsProperties.class })
+@ConditionalOnClass({ KmsClient.class, KmsAsyncClient.class })
+@AutoConfigureAfter({CredentialsProviderAutoConfiguration.class, RegionProviderAutoConfiguration.class})
 @ConditionalOnProperty(name = "spring.cloud.aws.kms.enabled", havingValue = "true", matchIfMissing = true)
 public class KmsAutoConfiguration {
 
-	public KmsAutoConfiguration() {
+	private KmsProperties properties;
 
+	public KmsAutoConfiguration(KmsProperties properties) {
+		this.properties = properties;
 	}
 
-	@ConditionalOnMissingBean
 	@Bean
-	public KmsClient kmsClient(AwsClientBuilderConfigurer awsClientBuilderConfigurer, ObjectProvider<AwsClientCustomizer<KmsClientBuilder>> configurer, KmsProperties properties) {
-		return awsClientBuilderConfigurer.configure(KmsClient.builder(), properties, configurer.getIfAvailable()).build();
+	@ConditionalOnMissingBean
+	public KmsClient kmsClient(AwsClientBuilderConfigurer awsClientBuilderConfigurer,
+			ObjectProvider<AwsClientCustomizer<KmsClientBuilder>> configurer,
+			ObjectProvider<AwsConnectionDetails> connectionDetails,
+			ObjectProvider<KmsClientCustomizer> kmsClientCustomizer,
+			ObjectProvider<AwsSyncClientCustomizer> awsSyncClientCustomizers) {
+		return awsClientBuilderConfigurer.configureSyncClient(KmsClient.builder(), this.properties,
+				connectionDetails.getIfAvailable(),
+				configurer.getIfAvailable(),
+				kmsClientCustomizer.orderedStream(),
+				awsSyncClientCustomizers.orderedStream())
+			.build();
 	}
 
-	@ConditionalOnMissingBean
 	@Bean
-	public KmsAsyncClient kmsAsyncClient(AwsClientBuilderConfigurer awsClientBuilderConfigurer, ObjectProvider<AwsClientCustomizer<KmsAsyncClientBuilder>> configurer,
-			KmsProperties properties) {
-		return awsClientBuilderConfigurer.configure(KmsAsyncClient.builder(), properties, configurer.getIfAvailable()).build();
+	@ConditionalOnMissingBean
+	public KmsAsyncClient kmsAsyncClient(AwsClientBuilderConfigurer awsClientBuilderConfigurer,
+			ObjectProvider<AwsClientCustomizer<KmsAsyncClientBuilder>> configurer,
+			ObjectProvider<AwsConnectionDetails> connectionDetails,
+			ObjectProvider<KmsAsyncClientCustomizer> kmsAsyncClientCustomizer,
+			ObjectProvider<AwsAsyncClientCustomizer> awsAsyncClientCustomizers) {
+		return awsClientBuilderConfigurer.configureAsyncClient(KmsAsyncClient.builder(), this.properties,
+				connectionDetails.getIfAvailable(),
+				configurer.getIfAvailable(),
+				kmsAsyncClientCustomizer.orderedStream(),
+				awsAsyncClientCustomizers.orderedStream()).build();
 	}
 }
